b3c6d9db3b0238afe097d061b3d626c2515b5a09
fring2012 <fring2012@163.com>
2018-05-15 10:56:14 +0800

    clean

--- app/src/main/java/com/example/administrator/ota_demo/presenter/code/Presenter.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.ota_demo.presenter.code;
-
-import com.example.administrator.ota_demo.view.activity.code.BaseActivity;
-
-public interface Presenter<T extends BaseActivity> {
-     void setView(T view);
-     T getView();
-}
